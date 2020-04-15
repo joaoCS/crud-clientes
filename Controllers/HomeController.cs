f44dfe2dd7fbba98d3964e2961215662afb9dcc7
JoaoCS <joaoantoniogba025@gmail.com>
2020-04-15 16:45:50 -0300

    Cômite único

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using asp19.Models;
+
+
+namespace asp19.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult ListaClientes()
+        {
+            return View(Dados.TodosOsClientes());
+        }
+
+        public IActionResult AdicionarClientes()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult AdicionarClientes(Cliente cliente)
+        {
+            Dados.AdicionarCliente(cliente);
+            return RedirectToAction("AdicionarClientes");
+        }
+
+        public IActionResult EditarCliente(int id)
+        { 
+            return View(Dados.DadosCliente(id));
+        }
+
+        [HttpPost]
+        public IActionResult EditarCliente(Cliente c)
+        {
+            Dados.EditarCliente(c);
+            return RedirectToAction("ListaClientes") ;
+        }
+
+        public IActionResult EliminarCliente(int id)
+        {
+            Dados.EliminarCliente(id);
+
+            return RedirectToAction("ListaClientes");
+        }
+    }
+}
---
+++ Models/Dados.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace asp19.Models
+{
+    public static class Dados
+    {
+        private static List<Cliente> LISTA_CLIENTES = new List<Cliente>();
+
+        public static List<Cliente> TodosOsClientes()
+        {
+            return LISTA_CLIENTES;
+        }
+
+        public static void AdicionarCliente(Cliente cliente)
+        {
+
+            int id = 0;
+
+            if (LISTA_CLIENTES.Count() != 0)
+                id = LISTA_CLIENTES.Last<Cliente>().Id + 1;
+
+            cliente.Id = id;
+
+            LISTA_CLIENTES.Add(cliente);
+
+        }
+
+        public static Cliente DadosCliente(int id)
+        {
+            return LISTA_CLIENTES.Where(i => i.Id == id).FirstOrDefault();
+        }
+
+        public static void EditarCliente(Cliente c)
+        {
+            LISTA_CLIENTES.First<Cliente>(i => i.Id == c.Id).Nome = c.Nome;
+            LISTA_CLIENTES.First<Cliente>(i => i.Id == c.Id).Telefone = c.Telefone;
+        }
+
+        public static void EliminarCliente(int id)
+        {
+            Cliente c = LISTA_CLIENTES.First<Cliente>(i => i.Id == id);
+            LISTA_CLIENTES.Remove(c);
+        }
+    }
+}
